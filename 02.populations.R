1cfe4284290d3471e476b79056b0b2791bfb7b6a
martinadepretto <146784368+martinadepretto@users.noreply.github.com>
2023-10-17 23:29:55 +0200

    Update 02.populations.R

--- 02.populations.R
+++ 02.populations.R
@@ -1,46 +1,46 @@
-#Code related to population ecology
+# Code related to population ecology
 
-#installing spatstat package
+# installing spatstat package
 install.packages("spatstat")
-#It's a software for point pattern analysis
+# It's a software for point pattern analysis
 
-#Code bei in the spatstat package
+# Code bei in the spatstat package
 bei
 
-#Plot bei
+# Plot bei
 plot(bei)
 
-#changing the dimensions of the points - cex
+# changing the dimensions of the points - cex
 plot(bei, cex=.2)
 
-#changing the symbol for the points - pch
+# changing the symbol for the points - pch
 plot(bei, cex=.2, pch=19)
 
-#Adding another dataset
+# Adding another dataset
 bei.extra
 plot(bei.extra)
 
-#Using only part of the dataset - $
+# Using only part of the dataset - $
 bei.extra$elev
 elevation <- bei.extra$elev
 plot(elevation)
 
 bei.extra[[1]]
 
-#Passing from points to a countinuous space - density
+# Passing from points to a countinuous space - density
 densitymap <- density(bei)
 plot(densitymap)
 points(bei, cex=.2)  #add points to the density map
 
-#Setting a variation of colors for the map
+# Setting a variation of colors for the map - colorRampPalette
 cl <- colorRampPalette(c("black", "red", "orange", "yellow"))(100)        #not blue, green and red all together
 plot(densitymap, col=cl)
 
-#Changing the gradient
+# Changing the gradient
 cl <- colorRampPalette(c("black", "red", "orange", "yellow"))(4)         #the yellow color impacts more
 plot(densitymap, col=cl)
 
-#Others colors found in Colors in R on Google
+# Others colors found in Colors in R on Google
 clnew <- colorRampPalette(c("dark blue", "blue", "light blue"))(100)
 plot(densitymap, col=clnew)
 
@@ -49,15 +49,16 @@ plot(bei.extra)
 elev <- bei.extra[[1]] # bei.extra$elev
 plot(elev)
 
-# multiframe
-par(mfrow=c(1,2))
+# multiframe to plot two or more characteristics together - mfrow
+par(mfrow=c(1,2))    #one row, two column
 plot(densitymap)
 plot(elev)
 
-par(mfrow=c(2,1))
+par(mfrow=c(2,1))   #two row, one column
 plot(densitymap)
 plot(elev)
 
+# plot three variables together  
 par(mfrow=c(1,3))
 plot(bei)
 plot(densitymap)
