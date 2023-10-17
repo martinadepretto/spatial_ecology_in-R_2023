f952771ddb2d9e855cf9b7f127b6519f9465b2e2
martinadepretto <146784368+martinadepretto@users.noreply.github.com>
2023-10-17 22:59:56 +0200

    Update 02.populations.R

--- 02.populations.R
+++ 02.populations.R
@@ -1,9 +1,64 @@
 #Code related to population ecology
 
 #installing spatstat package
+install.packages("spatstat")
+#It's a software for point pattern analysis
 
-#It's a software fot point pattern analysis
+#Code bei in the spatstat package
+bei
 
-#Code bei
+#Plot bei
+plot(bei)
 
-#plot bei.extra
+#changing the dimensions of the points - cex
+plot(bei, cex=.2)
+
+#changing the symbol for the points - pch
+plot(bei, cex=.2, pch=19)
+
+#Adding another dataset
+bei.extra
+plot(bei.extra)
+
+#Using only part of the dataset - $
+bei.extra$elev
+elevation <- bei.extra$elev
+plot(elevation)
+
+bei.extra[[1]]
+
+#Passing from points to a countinuous space - density
+densitymap <- density(bei)
+plot(densitymap)
+points(bei, cex=.2)  #add points to the density map
+
+#Setting a variation of colors for the map
+cl <- colorRampPalette(c("black", "red", "orange", "yellow"))(100)        #not blue, green and red all together
+plot(densitymap, col=cl)
+
+#Changing the gradient
+cl <- colorRampPalette(c("black", "red", "orange", "yellow"))(4)         #the yellow color impacts more
+plot(densitymap, col=cl)
+
+#Others colors found in Colors in R on Google
+clnew <- colorRampPalette(c("dark blue", "blue", "light blue"))(100)
+plot(densitymap, col=clnew)
+
+
+plot(bei.extra)
+elev <- bei.extra[[1]] # bei.extra$elev
+plot(elev)
+
+# multiframe
+par(mfrow=c(1,2))
+plot(densitymap)
+plot(elev)
+
+par(mfrow=c(2,1))
+plot(densitymap)
+plot(elev)
+
+par(mfrow=c(1,3))
+plot(bei)
+plot(densitymap)
+plot(elev)
