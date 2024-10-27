5df5443a10a2f4c24ee5888b21c100e4d920022d
martinadepretto <146784368+martinadepretto@users.noreply.github.com>
2024-10-27 19:04:14 +0100

    Update 07.external.data.code.R

--- 07.external.data.code.R
+++ 07.external.data.code.R
@@ -1,39 +1,41 @@
-# External data
+#External data
 
+#use terra package to bring the image
 library(terra)
 
-# set the working directory based on your path:
-# setwd("youtpath")
-# W***** users: C:\\path\Downloads -> C://path/Downloads
-# My own:
-setwd("~/Downloads")
-naja <- rast("najafiraq_etm_2003140_lrg.jpg")  # like in im.import()
+#We download the image of Najaf sea from NASA Earth Observatory
+#Set the working directory based on your path to be able to import the image in R,
+# with the "" because it's external to R
+setwd("/Users/marti/OneDrive/Desktop")
+#use rast function because the image is a raster
+naja <- rast("naja2003.jpg")
+#plot the image using the RGB model
+plotRGB(naja, r=1, g=2, b=3) 
 
-plotRGB(naja, r=1, g=2, b=3) # im-plotRGB
-
-# Exercise: Download the second image from the same site and import it in R
-najaaug <- rast("najafiraq_oli_2023219_lrg.jpg")
+#Exercise: Download the second image from the same site and import it in R
+# we don't need to reset the working directory now
+najaaug <- rast("naja2023.jpg")
 plotRGB(najaaug, r=1, g=2, b=3)
 
+#plot both images together
 par(mfrow=c(2,1))
-plotRGB(naja, r=1, g=2, b=3) # im-plotRGB
+plotRGB(naja, r=1, g=2, b=3)
 plotRGB(najaaug, r=1, g=2, b=3)
 
-# multitemporal change detection
+#Multitemporal change detection
 najadif = naja[[1]] - najaaug[[1]] 
 cl <- colorRampPalette(c("brown", "grey", "orange")) (100)
 plot(najadif, col=cl)
+#highest difference between the two is highlighted in orange
 
-# Download your own preferred image:
-typhoon <- rast("mawar_vir2_2023144_lrg.jpg")
-
+#Download an image about typhoon Mawar from Earth Observatory
+typhoon <- rast("typhoon.jpg")
+#plot it with different colors
 plotRGB(typhoon, r=1, g=2, b=3)
 plotRGB(typhoon, r=2, g=1, b=3)
 plotRGB(typhoon, r=3, g=2, b=1)
 
-
-# The Mato Grosso image can be downloaded directly from EO-NASA:
-
-mato <- rast("matogrosso_l5_1992219_lrg.jpg")
-plotRGB(mato, r=1, g=2, b=3) 
-plotRGB(mato, r=2, g=1, b=3) 
+# The Mato Grosso image can be downloaded directly from EO-NASA
+mato <- rast("matogrosso1992.jpg")
+plotRGB(mato, r=1, g=2, b=3) #the red color represents the red band
+plotRGB(mato, r=2, g=1, b=3) #the green color represents the red band
